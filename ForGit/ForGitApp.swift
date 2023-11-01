4fec6dc912f7b37b009ca4ada737085d81ca81fa
Андрей Петров <andrejpetrov@MacBook-Air-Andrej.local>
2023-11-01 21:44:28 +0300

    first commit

---
+++ ForGit/ForGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForGitApp.swift
+//  ForGit
+//
+//  Created by Андрей Петров on 01.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ForGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
